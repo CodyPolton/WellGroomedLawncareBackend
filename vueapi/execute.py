3457a87f24e99525e6042de3c4d8c68c4ae660aa
Cody Polton <CPolton@ShelterInsurance.com>
2020-02-19 06:41:43 -0600

    Finishing up close timesheets job

--- vueapi/execute.py
+++ vueapi/execute.py
@@ -20,8 +20,9 @@ import scheduler_jobs
 scheduler.add_jobstore(DjangoJobStore(), 'default')
 #scheduler.add_job(scheduler_jobs.FirstCronTest, 'interval', seconds=100)
 
-#scheduler.add_job(scheduler_jobs.test_job, 'interval', id='Test', seconds=5)
+scheduler.add_job(scheduler_jobs.close_timesheets, 'interval', id='Test', seconds=5)
 scheduler.add_job(scheduler_jobs.set_payperiod, 'cron', id='Set PayPeriod', day_of_week='2', hour=1 )
+#scheduler.add_job(scheduler_jobs.close_timesheets, 'cron', id='Clock out anyone still clocked in', hour=23, minute=59)
 
 scheduler.start()
 
--- vueapi/scheduler_jobs.py
+++ vueapi/scheduler_jobs.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 from pytz import utc
-from account.models import PayPeriod
-from datetime import date, datetime, timedelta
-import time
+from account.models import PayPeriod, Timesheet
+from datetime import date, datetime, timedelta, time
 
 def FirstCronTest():
 	
@@ -20,3 +19,16 @@ def set_payperiod():
 	b = PayPeriod(startDate=start, endDate=end)
 	b.save()
 	print("Set the pay period")
+
+def close_timesheets():
+	print('timesheets')
+	opentimesheets = Timesheet.objects.filter(end_time = None)
+	now = datetime.now().time()
+	time = str(now.hour) + ':' + str(now.minute) + ':' + str(now.second)
+	print(time)
+	for timesheet in opentimesheets:
+
+		timesheet.end_time = time
+		hours = (datetime.strptime(str(timesheet.end_time),'%H:%M:%S') - datetime.strptime(str(timesheet.start_time),'%H:%M:%S'))
+		print(hours)
+		#timesheet.save()
